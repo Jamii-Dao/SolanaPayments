f186af0bfc7c827de17d03ab14b24915229fe51c
448-OG <superuser@448.africa>
2024-08-01 16:12:25 +0300

    References need to be converted to and from base58

--- lib/src/references.rs
+++ lib/src/references.rs
@@ -1,15 +1,16 @@
 use core::fmt;
+use std::hash::Hash;
 
 use constant_time_eq::constant_time_eq_n;
 
-use crate::RandomBytes;
+use crate::{RandomBytes, SolanaPayResult, Utils};
 
 #[derive(Clone, Copy)]
-pub struct Reference<const N: usize>([u8; N]);
+pub struct Reference([u8; 32]);
 
-impl<const N: usize> Reference<N> {
+impl Reference {
     pub fn new() -> Self {
-        let random = RandomBytes::<N>::new();
+        let random = RandomBytes::new();
 
         Self(random.expose_owned())
     }
@@ -18,43 +19,61 @@ impl<const N: usize> Reference<N> {
         blake3::hash(&self.0)
     }
 
-    pub fn expose_owned(&self) -> [u8; N] {
+    pub fn expose_owned(&self) -> [u8; 32] {
         self.0
     }
 
-    pub fn expose(&self) -> &[u8; N] {
+    pub fn expose(&self) -> &[u8; 32] {
         &self.0
+    }
+
+    pub fn to_base58(&self) -> String {
+        Utils::to_base58(&self.0)
+    }
+
+    pub fn from_base58(base58_str: &str) -> SolanaPayResult<Self> {
+        let outcome = Utils::from_base58(base58_str)?;
+
+        Ok(Self(outcome))
     }
 }
 
-impl<const N: usize> fmt::Debug for Reference<N> {
+impl fmt::Debug for Reference {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Reference({})", self.to_hash())
     }
 }
 
-impl<const N: usize> fmt::Display for Reference<N> {
+impl fmt::Display for Reference {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.to_hash().to_hex().as_str())
     }
 }
 
-impl<const N: usize> PartialEq for Reference<N> {
+impl PartialEq for Reference {
     fn eq(&self, other: &Self) -> bool {
         constant_time_eq_n(self.expose(), other.expose())
     }
 }
 
-impl<const N: usize> Eq for Reference<N> {}
+impl Eq for Reference {}
 
-impl<const N: usize> AsRef<[u8]> for Reference<N> {
+impl AsRef<[u8]> for Reference {
     fn as_ref(&self) -> &[u8] {
         self.expose()
     }
 }
 
-impl<const N: usize> Default for Reference<N> {
+impl Default for Reference {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl Hash for Reference {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.expose().iter().for_each(|byte| {
+            state.write_u8(*byte);
+        });
     }
 }
